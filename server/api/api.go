c083e8c573d394a1d68e3bfda8464f863e4f2015
phantomgoose <alex.txt@gmail.com>
2020-03-31 15:02:12 -0700

    add random delay to server response to simulate async calls

--- server/api/api.go
+++ server/api/api.go
@@ -2,15 +2,22 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
+// Handler replies with a Hello after a random duration
 func Handler() http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Content-Type", "application/json")
 
+		r := rand.Intn(5000)
+		time.Sleep(time.Duration(r) * time.Millisecond)
+
 		body, err := json.Marshal(map[string]interface{}{
-			"data": "Hello, world!!",
+			"data": fmt.Sprint("Hello from Go! Server randomly delayed response by ", r, " milliseconds"),
 		})
 
 		if err != nil {
